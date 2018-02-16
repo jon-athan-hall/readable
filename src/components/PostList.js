0dca043410812f23bbe67fd1a06afda1e3be7946
Jonathan Hall <jonathan.edward.hall@gmail.com>
2018-02-15 22:03:01 -0500

    feat: PostList showing all required info
    I added the edit and delete links to the post list, as well the
    comment count for each post in the list.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -11,20 +11,38 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 /**
+ * Actions
+ */
+import { removePost } from '../actions/posts'
+
+/**
  * Components
  */
 import VoteMechanism from './VoteMechanism'
 
 class PostList extends Component {
+  constructor(props, context) {
+    super(props, context)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e, postId) {
+    e.preventDefault();
+    this.props.removePost(postId)
+  }
+
   render() {
     return (
       <ul className="post-list">
         {this.props.posts.map((post) => (
           <li key={post.id} className="post-list__item">
             <VoteMechanism id={post.id} score={post.voteScore} />
+            <h1><Link to={`/posts/${post.id}`} className="post-list__title">{post.title}</Link></h1>
+            <h2 className="post-list__byline">submitted by <span className="post-list__author">{post.author}</span></h2>
             <div className="post-list__info">
-              <Link to={`/posts/${post.id}`} className="post-list__title">{post.title}</Link>
-              <h2 className="post-list__byline">submitted by <span className="post-list__author">{post.author}</span></h2>
+              <span className="post-list__comments">{post.commentCount} comment{post.commentCount !== 1 && 's'}</span>
+              <Link to={`/posts/${post.id}/edit`} className="post-list__link">Edit</Link>
+              <a onClick={(e) => this.handleClick(e, post.id)} className="post-list__link">Delete</a>
             </div>
           </li>
         ))}
@@ -49,7 +67,13 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    removePost: (postId) => dispatch(removePost(postId))
+  }
+}
+
 /**
  * Connect this Component to the Redux Store.
  */
-export default connect(mapStateToProps, null)(PostList)
+export default connect(mapStateToProps, mapDispatchToProps)(PostList)
